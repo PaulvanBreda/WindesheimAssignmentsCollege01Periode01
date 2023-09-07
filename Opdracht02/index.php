af95984f20a9dd121a76b28c58f4af34c24e8234
paulroc <54845508+paulroc@users.noreply.github.com>
2023-09-07 13:04:39 +0200

    2de is (hopelijk) klaar.

---
+++ Opdracht02/index.php
@@ -0,0 +1,41 @@
+<?php
+// String (tekst)
+$name = "Paul van Breda";
+
+// Integer (geheel getal)
+$age = 21;
+
+// Float (kommagetal)
+$height = 1.84;
+
+// Boolean (waar of onwaar)
+$isStudent = true;
+
+// Array (lijst van waarden)
+$grades = [85, 92, 78, 95];
+
+// Associative array (lijst van sleutel-waardeparen)
+$studentInfo = [
+    "name" => "Paul van Breda",
+    "age" => 21,
+    "email" => "s1200548@student.windesheim.nl"
+];
+
+// Null (geen waarde)
+$address = null;
+
+// Object (instantie van een klasse)
+class Course {
+    public $courseName = "AD Softwaredevelopment";
+    public $credits = 3;
+}
+
+$programmingCourse = new Course();
+
+// Resource (bronhandle)
+$fileHandle = fopen("example.txt", "r");
+
+// Callable (aanroepbare functie)
+$functionReference = function ($x, $y) {
+    return $x + $y;
+};
